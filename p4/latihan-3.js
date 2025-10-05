cc5cba1f36689542a274aca275d2bc62b6cb0d95
pidzal <muftyhafidzal2@gmail.com>
2025-10-05 16:05:15 +0800

    Add files via upload

--- p4/latihan-3.js
+++ p4/latihan-3.js
@@ -1,26 +1,17 @@
-// File: script.js (untuk Latihan 3)
-
 function cekKelulusan() {
-    console.clear(); // bersihkan console sebelum jalan
-
-    // 1. Ambil nilai dari input teks
+    console.clear();
     let nilai = document.getElementById("Nilai").value;
 
-    // Cek apakah input kosong
     if (nilai === "") {
-        console.log("⚠️ Silakan masukkan nilai terlebih dahulu! ⚠️");
+        console.log("⚠️ Silakan masukkan nilai terlebih dahulu! ");
         return;
     }
 
-    // Konversi nilai menjadi tipe Number (Integer)
     nilai = parseInt(nilai);
 
-    // 2. Terapkan logika if-else
     if (nilai >= 70) {
-        // Blok ini berjalan jika kondisi benar (lulus)
         console.log("✅ SELAMAT! Nilai Anda " + nilai + ", Anda LULUS. ✅");
     } else {
-        // Blok ini berjalan jika kondisi salah (tidak lulus)
         console.log("❌ Maaf, nilai Anda " + nilai + ", Anda BELUM LULUS. ❌");
     }
 }
