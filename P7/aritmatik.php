282d2afbd0e47fb5f2fac8fcbb49c28847ddf8cf
pidzal <muftyhafidzal2@gmail.com>
2025-10-26 23:10:27 +0800

    Add files via upload

---
+++ P7/aritmatik.php
@@ -0,0 +1,17 @@
+<?php
+$penjumlahan = 2 + 4;
+$pengurangan = 6 - 2;
+$perkalian = 5 * 3;
+$pembagian = 15 / 3;
+$modulus = 5 % 2;
+echo 'hasil 2 + 4: '. $penjumlahan.'<br>';
+// hasil 2 + 4: 6
+echo 'hasil 6 - 2: '. $pengurangan.'<br>';
+// hasil 6 - 2: 4
+echo 'hasil 5 * 3: '. $perkalian.'<br>';
+// hasil 5 * 3: 15
+echo 'hasil 15 / 3: '. $pembagian.'<br>';
+// hasil 15 / 3: 5
+echo 'hasil 5 % 2: '. $modulus.'<br>';
+// hasil 5 % 2: 1
+?>
---
+++ P7/assignment-by-value.php
@@ -0,0 +1,12 @@
+<?php
+$a = 20; 
+$b = 15;
+$c = 5;
+
+echo "\$a = $a, \$b = $b, \$c = $c";
+echo "<br />";
+// hasil proses: $a = 20, $b = 15, 4c = 5
+$a = $b = $c+5;
+echo "\$a = $a, \$b = $b, \$c = $c";
+// hasil proses: $a = 10, $b = 10, 4c = 5
+?>
