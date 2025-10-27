241950589bab2de505befd225c0092ea13054603
pidzal <muftyhafidzal2@gmail.com>
2025-10-27 20:38:57 +0800

    Add files via upload

--- P7/soal-1.php
+++ P7/soal-1.php
@@ -1,7 +1,7 @@
-<?php
-$saldoAwal = 1000000;
-$bunga = 0.0025; // 0.25%
-$bulan = 11;
-$saldoAkhir = $saldoAwal + ($saldoAwal * $bunga * $bulan);
-echo "Saldo akhir setelah ".$bulan." bulan adalah : Rp. ".$saldoAkhir.".-";
-?>
+    <?php
+        $saldoAwal = 1000000; // variabel saldo awal yang berisi 1.000.000
+        $bunga = 0.0025; // variabel bunga yang berisi 0.0025 atau 0,25%
+        $bulan = 11; // variabel bulan yang berisi 11 yang menandakan 11 bulan
+        $saldoAkhir = $saldoAwal + ($saldoAwal * $bunga * $bulan); // saldo akhir dihitung dengan menambahkan saldo awal dengan hasil dari (saldo awal × bunga × bulan)
+        echo "Saldo akhir setelah ".$bulan." bulan adalah : Rp. ".$saldoAkhir.".-"; // akan menampilkan saldo akhir setelah 11 bulan dan saldo akhir nya itu berapa
+    ?>
--- P7/soal-2.php
+++ P7/soal-2.php
@@ -1,28 +1,28 @@
 <?php
-$jumlahUang = 1575250;
+    $jumlahUang = 1575250; // variabel jumlahUang yang berisi total uang sebesar Rp 1.575.250
 
-$a = intval($jumlahUang / 100000);
-$sisa = $jumlahUang % 100000;
+    $a = intval($jumlahUang / 100000); // menghitung berapa lembar uang Rp 100.000 dari total uang
+    $sisa = $jumlahUang % 100000; // menghitung sisa uang setelah diambil pecahan Rp 100.000
 
-$b = intval($sisa / 50000);
-$sisa = $sisa % 50000;
+    $b = intval($sisa / 50000); // menghitung berapa lembar uang Rp 50.000 dari sisa uang
+    $sisa = $sisa % 50000; // menghitung sisa uang setelah diambil pecahan Rp 50.000    
 
-$c = intval($sisa / 20000);
-$sisa = $sisa % 20000;
+    $c = intval($sisa / 20000); // menghitung berapa lembar uang Rp 20.000 dari sisa uang
+    $sisa = $sisa % 20000; // menghitung sisa uang setelah diambil pecahan Rp 20.000
 
-$d = intval($sisa / 5000);
-$sisa = $sisa % 5000;
+    $d = intval($sisa / 5000); // menghitung berapa lembar uang Rp 5.000 dari sisa uang
+    $sisa = $sisa % 5000; // menghitung sisa uang setelah diambil pecahan Rp 5.000
 
-$e = intval($sisa / 100);
-$sisa = $sisa % 100;
+    $e = intval($sisa / 100); // menghitung berapa keping uang Rp 100 dari sisa uang
+    $sisa = $sisa % 100; // menghitung sisa uang setelah diambil pecahan Rp 100
 
-$f = intval($sisa / 50);
-$sisa = $sisa % 50;
+    $f = intval($sisa / 50); // menghitung berapa keping uang Rp 50 dari sisa uang
+    $sisa = $sisa % 50; // menghitung sisa uang setelah diambil pecahan Rp 50
 
-echo "Jumlah Rp. 100.000 : ".$a."<br />";
-echo "Jumlah Rp. 50.000 : ".$b."<br />";
-echo "Jumlah Rp. 20.000 : ".$c."<br />";
-echo "Jumlah Rp. 5.000 : ".$d."<br />";
-echo "Jumlah Rp. 100 : ".$e."<br />";
-echo "Jumlah Rp. 50 : ".$f."<br />";
-?>
+    echo "Jumlah Rp. 100.000 : ".$a."<br />"; // menampilkan jumlah lembar uang Rp 100.000
+    echo "Jumlah Rp. 50.000 : ".$b."<br />"; // menampilkan jumlah lembar uang Rp 50.000
+    echo "Jumlah Rp. 20.000 : ".$c."<br />"; // menampilkan jumlah lembar uang Rp 20.000
+    echo "Jumlah Rp. 5.000 : ".$d."<br />"; // menampilkan jumlah lembar uang Rp 5.000
+    echo "Jumlah Rp. 100 : ".$e."<br />"; // menampilkan jumlah keping uang Rp 100
+    echo "Jumlah Rp. 50 : ".$f."<br />"; // menampilkan jumlah keping uang Rp 50
+?> 
